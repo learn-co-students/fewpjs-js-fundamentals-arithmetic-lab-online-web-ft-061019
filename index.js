2606b7ec03b72993f7c8c8ce48c588ce3663f4f2
Evan_Ratzlaff <ratzlaff92@gmail.com>
2019-09-28 18:50:03 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
+multiply
 
-// Write your code here
+let random = Math.random()
+random
+
+let mod = 40 % 6
+mod
+
+// let max = [12, 13, 18, 20, 8]
+let max = Math.max(12, 13, 18, 9, 20)
+max
